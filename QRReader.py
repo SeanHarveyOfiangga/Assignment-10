7c4174bf3bd7f6e8981e17b48f75172a616cd628
SeanHarveyOfiangga <seanharveyofiangga@gmail.com>
2022-02-19 22:28:48 +0800

    Imported all needed extensions

---
+++ QRReader.py
@@ -0,0 +1,31 @@
+#Assignment 10
+
+#Contact Tracing App
+#	- Create a python program that will read QRCode using your webcam
+#	- You may use any online QRCode generator to create QRCode
+#	- All personal data are in QRCode 
+#	- You may decide which personal data to include
+#	- All data read from QRCode should be stored in a text file including the date and time it was read
+
+#Note: 
+#	- Search how to generate QRCode
+#	- Search how to read QRCode using webcam
+#	- Search how to create and write to text file
+#	- Your source code should be in github before Feb 19
+#	- Create a demo of your program (1-2 min) and send it directly to my messenger.
+
+import cv2
+import numpy as np
+from pyzbar.pyzbar import decode
+from datetime import datetime
+
+
+
+
+
+
+
+
+
+
+
